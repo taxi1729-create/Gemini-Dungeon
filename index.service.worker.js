107b4e34d45a34843dad568553b6568f94b1ced2
taxi1729-create <magnet1729@gmail.com>
2026-03-11 22:49:54 +0900

    modify

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773228182|3774818497';
+const CACHE_VERSION = '1773236854|175001345';
 /** @type {string} */
 const CACHE_PREFIX = 'Gemini-Dungeon-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
