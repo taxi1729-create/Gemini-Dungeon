b79eab5545780c541a9be0ad06bc5e8320664650
taxi1729-create <magnet1729@gmail.com>
2026-03-24 01:20:48 +0900

    mo

---
+++ core/map/present/current_status 2.gd
@@ -0,0 +1,13 @@
+extends Node
+# プロジェクト設定で "CurrentStatus" としてAutoloadに登録してください
+var current_area_id = "cave" # 今いるエリアのID
+var encounter_level = 1      # 敵の強さなどの指標
+var allies = []
+var enemies = []
+
+func sync_from_init():
+	allies = [
+		{"id": "knight", "hp": 20, "max_hp": 20, "def": 1, "spd": 3, "ap": 3, "color": Color.GREEN, "status_effects": {},"flip_h": true,"icon":"sword_icon"},
+		{"id": "witch", "hp": 15, "max_hp": 15, "def": 0, "spd": 6, "ap": 3, "color": Color.PURPLE, "status_effects": {},"flip_h": false,"icon":"potion_icon"}
+	]
+	enemies = []
